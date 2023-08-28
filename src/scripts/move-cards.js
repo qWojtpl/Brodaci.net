61484b8cc2aa00480ea7dd08423222d9cf1479a0
qWojtpl <71585628+qWojtpl@users.noreply.github.com>
2023-08-28 14:27:52 +0200

    Added move cards
    - Added move cards to main page

---
+++ src/scripts/move-cards.js
@@ -0,0 +1,50 @@
+
+const cards = document.getElementsByClassName("move-card");
+const buttons = document.getElementsByClassName("move-card-button");
+
+hideAllCards();
+selectCard(buttons[0]);
+
+let blocked = false;
+let cardIndex = 0;
+
+let update = setInterval(() => {
+    if(cardIndex == buttons.length) {
+        cardIndex = 0;
+    }
+    if(!blocked) {
+        selectCard(buttons[cardIndex++], false);
+    } else {
+        blocked = false;
+    }
+}, 10000);
+
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", () => {
+        selectCard(buttons[i], true);
+    });
+}
+
+function hideAllCards() {
+    for(let i = 0; i < cards.length; i++) {
+        cards[i].style.display = "none";
+    }
+}
+
+function selectCard(element, manual) {
+    if(manual) {
+        blocked = true;
+    }
+    let index = 0;
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].style.borderRadius = "50%";
+        buttons[i].style.width = "4vw";
+        if(buttons[i] == element) {
+            index = i;
+        }
+    }
+    element.style.borderRadius = "3vw";
+    element.style.width = "6vw";
+    hideAllCards();
+    cards[index].style.display = "";
+}
